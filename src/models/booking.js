c1a362fc7c4445570ae2da32268eba64c893b015
Suhaib Khan <suhaib0900@gmail.com>
2024-11-16 21:39:23 +0000

    Booking Model Added

---
+++ src/models/booking.js
@@ -0,0 +1,60 @@
+'use strict';
+const { Model } = require('sequelize');
+const { ENUMS } = require('../utils/common/index');
+// PENDING: 'pending',
+// CONFIRMED: 'confirmed',
+// CANCELLED: 'cancelled',
+// INITIATED: 'initiated',
+const { BOOKING_STATUS } = ENUMS;
+const { PENDING, CONFIRMED, CANCELLED, INITIATED } = BOOKING_STATUS;
+module.exports = (sequelize, DataTypes) => {
+  class Booking extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Booking.init(
+    {
+      flightId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+
+      status: {
+        type: DataTypes.ENUM,
+        values: [PENDING, CONFIRMED, CANCELLED, INITIATED],
+        defaultValue: INITIATED,
+        allowNull: false,
+      },
+      totalCost: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      noOfSeats: {
+        //no of seats booked by user in a booking
+        type: DataTypes.INTEGER,
+        defaultValue: 1,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Booking',
+    }
+  );
+  return Booking;
+};
+/** 
+ totalSeats in flight service flight table refers to total seats left in a flight
+  noOfSeats in booking service booking table refers to no of seats booked by user in a booking
+  capacity in flight service airplane table refers to total capacity of the airplane
+*/
